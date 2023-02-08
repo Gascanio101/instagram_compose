1a64fd7c4871c1f2bdbb02c86907dc2eff880f56
Gabriel Ascanio <80049275+Gascanio101@users.noreply.github.com>
2023-02-08 17:15:02 +0100

    Added activity.finish() functionality on close button

--- app/src/main/java/com/example/instagramcompose/LoginScreen.kt
+++ app/src/main/java/com/example/instagramcompose/LoginScreen.kt
@@ -1,15 +1,17 @@
 package com.example.instagramcompose
 
+import android.app.Activity
 import android.preference.PreferenceActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,31 +19,31 @@ import com.example.instagramcompose.ui.theme.InstagramComposeTheme
 
 @Composable
 fun LoginScreen() {
-    Column() {
-        LoginHeader()
+    Box(Modifier.fillMaxSize()) {
+        LoginHeader(Modifier.align(Alignment.TopEnd))
         LoginContent()
     }
 }
 
 @Composable
 fun LoginContent() {
-    Column(Modifier.fillMaxSize()) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp)) {
         Image(
             painterResource(id = R.drawable.ig_text_logo),
             contentDescription = "Instagram's text logo"
         )
-
-
     }
 }
 
 @Composable
-fun LoginHeader() {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .padding(8.dp), horizontalArrangement = Arrangement.End
-    ) {
-        Icon(imageVector = Icons.Default.Close, contentDescription = "Close App")
-    }
+fun LoginHeader(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "Close App",
+        modifier.clickable { activity.finish() }
+    )
 }
